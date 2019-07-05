d0a1b00d13a14287804b765f92b0b31cb0287fa1
mrunal77 <mtmeshram77@outlook.com>
2019-07-06 00:08:17 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt='Mrunal'></img>
         <h1>          
             MRUNAL MESHRAM        
         </h1>
