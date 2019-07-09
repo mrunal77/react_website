8182a7583e102101f46de9a39d983fc85fac4dd9
mrunal77 <mtmeshram77@outlook.com>
2019-07-09 17:19:28 +0530

    Effect Tada

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import bannerImage from './Assets/Images/bannerImage.jpg'
 import './social_icons/social'
 import Social from './social_icons/social';
-import Zoom from 'react-reveal/Zoom';
+import Tada from 'react-reveal/Tada';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Zoom>
+        <Tada>
           <header className="App-header">
             <card className="card">
               <img className="rounded-circle" src={bannerImage} alt="Mrunal" />
@@ -20,7 +20,7 @@ class App extends Component {
               </div>
             </card>
           </header>
-        </Zoom>
+        </Tada>
       </div >
     );
   }
