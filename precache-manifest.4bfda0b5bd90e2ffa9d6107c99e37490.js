8d75438d829b47840d9016383af2e9b1f8b7458c
mrunal77 <mtmeshram77@outlook.com>
2020-05-01 13:28:00 +0530

    Updates

---
+++ precache-manifest.4bfda0b5bd90e2ffa9d6107c99e37490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa9b3852232f607d1dd0e93cb53fea1",
+    "url": "/react_website/index.html"
+  },
+  {
+    "revision": "9c51928bbe265cf85405",
+    "url": "/react_website/static/css/main.9513dbf3.chunk.css"
+  },
+  {
+    "revision": "7f8c5079851275ce9065",
+    "url": "/react_website/static/js/2.fa591530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_website/static/js/2.fa591530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c51928bbe265cf85405",
+    "url": "/react_website/static/js/main.3626f9fb.chunk.js"
+  },
+  {
+    "revision": "9284dd28ea9babf98f80",
+    "url": "/react_website/static/js/runtime-main.c2d21e2f.js"
+  },
+  {
+    "revision": "eee05f8bbc6371426ab45f6ab36305a4",
+    "url": "/react_website/static/media/bannerImage.eee05f8b.jpg"
+  }
+]);
